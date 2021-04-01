ae33402a984d23a2b9b725c788e0436fc58808b4
yujinjiang <964494258@qq.com>
2021-04-01 20:03:15 +0800

    首页完成

--- search-engine/src/App.vue
+++ search-engine/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <router-view/>
+            <router-view/>
   </div>
 </template>
 
 <style>
+html,body{
+  margin:0;
+  padding: 0;
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 
 #nav {
--- search-engine/src/views/Home.vue
+++ search-engine/src/views/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <div class="home">
-    
-  </div>
+  <el-main>
+    <el-input placeholder="请输入内容" v-model="input" class="search-input">
+      <template slot="prepend">
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :file-list="fileList"
+          :auto-upload="false"
+          :show-file-list="false"
+          :on-change="searchImage"
+        >
+          <i class="el-icon-picture-outline"></i>
+        </el-upload>
+      </template>
+      <el-button
+        slot="append"
+        icon="el-icon-search"
+        @click="search"
+      ></el-button>
+    </el-input>
+  </el-main>
 </template>
 
 <script>
-
-
 export default {
-  name: "Home"
-}
+  name: "Home",
+  data: () => ({
+    input: "",
+    fileList:[],
+  }),
+  methods: {
+    search() {
+      console.log(this.input);
+    },
+    searchImage(file) {
+      console.log(file) ;
+    }
+  },
+};
 </script>
+
+<style scoped>
+.search-input {
+  width: 600px;
+  margin-top: 260px;
+}
+</style>
