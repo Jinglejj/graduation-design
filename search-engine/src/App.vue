98e62e41874649badb752980d3e2827a71e8accf
Ys <1250484624@qq.com>
2021-04-03 09:48:22 +0800

    音频测试

--- search-engine/src/App.vue
+++ search-engine/src/App.vue
@@ -1,9 +1,31 @@
 <template>
   <div id="app">
             <router-view/>
+      <VueAudio :audio-info="info"></VueAudio>
   </div>
 </template>
+<script>
+    import VueAudio from "./components/audio/VueAudio";
+    export default {
+        name: "app",
+        data() {
+            return {
+                info:{
+                    src: "http://106.14.137.176:8080/blog/static/1.mp3",
+                    currentTime: 0,//音频当前时间
+                    endTime: 258,//音频结束时间
+                    title:'来自个人服务器数据比较慢',
+                    text:'测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所'
+                }
+            }
+        },
+        methods: {},
+        components: {VueAudio},
+        mounted() {
 
+        }
+    }
+</script>
 <style>
 html,body{
   margin:0;
