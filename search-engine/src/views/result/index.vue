dffc80ace5197e668138b7baf741f864e0287723
yujinjiang <964494258@qq.com>
2021-04-03 18:38:38 +0800

    修改文件结构

---
+++ search-engine/src/views/result/index.vue
@@ -0,0 +1,33 @@
+<template >
+  <el-main>
+    <SearchInput class="search-input" :input.sync="input" @search="search" @uploadFile="searchImage"/>
+  </el-main>
+</template>
+
+<script>
+import SearchInput from '@/components/SearchInput'
+export default {
+  name: "Result",
+  components:{
+    SearchInput
+  },
+  data: () => ({
+    input: "",
+    imageList:[]
+  }),
+  methods: {
+    search() {
+      console.log(this.input);
+    },
+    searchImage(file) {
+      console.log(file) ;
+    }
+  },
+};
+</script>
+
+<style scoped>
+.search-input {
+ 
+}
+</style>
