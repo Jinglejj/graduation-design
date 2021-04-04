74c98e857a9c339f306bfd52531d554f72dba019
yujinjiang <964494258@qq.com>
2021-04-04 22:26:45 +0800

    首页

--- search-engine/src/views/home/index.vue
+++ search-engine/src/views/home/index.vue
@@ -9,13 +9,18 @@
         @uploadFile="searchImage"
       />
     </div>
-    <div v-if="imageList.length" class="image-container">
-      <ShowImage
+    <div  class="result-container">
+      <template v-if="imageList.length">
+              <ShowImage 
         v-for="url in imageList"
         :key="url"
         :image-url="url"
         :image-list="imageList"
       />
+      </template>
+      <template v-else>
+            <VueAudio :audio-info="info" />
+      </template>
     </div>
   </el-main>
 </template>
@@ -23,22 +28,24 @@
 <script>
 import SearchInput from "@/components/SearchInput";
 import ShowImage from "@/components/ShowImage";
-const imageData= [
-          "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
-          "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
-          "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
-          "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
-          "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
-          "https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
-          "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg",
-          "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg",
-          "https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg",
-        ]
+import VueAudio from "@/components/audio/VueAudio";
+const imageData = [
+  "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
+  "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
+  "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
+  "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
+  "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
+  "https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
+  "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg",
+  "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg",
+  "https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg",
+];
 export default {
   name: "Home",
   components: {
     SearchInput,
     ShowImage,
+    VueAudio
   },
   data: () => ({
     input: "",
@@ -47,26 +54,36 @@ export default {
       transform: `translate(-50%, 240px)`,
     },
     imageList: [],
+    info: null,
   }),
   methods: {
     search() {
       console.log(this.input);
+      this.imageList = [];
       this.style = {
         left: 0,
         transform: `translate(0, 0)`,
       };
       setTimeout(() => {
-        this.imageList = imageData
+        this.info = {
+          src: "http://106.14.137.176:8080/blog/static/1.mp3",
+          currentTime: 0, //音频当前时间
+          endTime: 258, //音频结束时间
+          title: "来自个人服务器数据比较慢",
+          text:
+            "测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所",
+        };
       }, 400);
     },
     searchImage(file) {
       console.log(file);
+      this.info = null;
       this.style = {
         left: 0,
         transform: `translate(0, 0)`,
       };
       setTimeout(() => {
-        this.imageList = imageData
+        this.imageList = imageData;
       }, 400);
     },
   },
@@ -83,7 +100,7 @@ export default {
     transition: all 0.5s;
   }
 }
-.image-container {
+.result-container {
   display: flex;
   flex-wrap: wrap;
 }
