2abffb327e246fb29dc6e0f4a1634ae80f43b892
yujinjiang <964494258@qq.com>
2021-04-05 13:09:01 +0800

    删除无用代码

--- search-engine/src/App.vue
+++ search-engine/src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <div id="app">
-            <router-view/>
-      <!-- <VueAudio :audio-info="info"></VueAudio> -->
+    <router-view />
   </div>
 </template>
 <script>
-    // import VueAudio from "./components/audio/VueAudio";
-    export default {
-        name: "app",
-        data() {
-            return {
-                info:{
-                    src: "http://106.14.137.176:8080/blog/static/1.mp3",
-                    currentTime: 0,//音频当前时间
-                    endTime: 258,//音频结束时间
-                    title:'来自个人服务器数据比较慢',
-                    text:'测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所测试数所所所所所所所所所所所所'
-                }
-            }
-        },
-        methods: {},
-        // components: {VueAudio},
-        mounted() {
+export default {
+  name: "app",
 
-        }
-    }
+  methods: {},
+  mounted() {},
+};
 </script>
 <style>
-html,body{
-  margin:0;
+html,
+body {
+  margin: 0;
   padding: 0;
   height: 100%;
 }
@@ -40,16 +26,4 @@ html,body{
   height: 100%;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
