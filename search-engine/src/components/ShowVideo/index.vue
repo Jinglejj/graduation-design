5bf2239ad3884aa65c0cd4e9a8c47d98b9677504
yujinjiang <964494258@qq.com>
2021-04-09 14:55:47 +0800

    合并

---
+++ search-engine/src/components/ShowVideo/index.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "ShowVideo",
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  render(e, { props }) {
+    const { url } = props;
+    return (
+      <el-card>
+        <video src={url} controls/>
+      </el-card>
+    );
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
