77a01dd85d7d32b13837f03012842e51b7a2061f
Ys <1250484624@qq.com>
2021-04-13 10:49:36 +0800

    添加axios

--- search-engine/src/main.js
+++ search-engine/src/main.js
@@ -4,6 +4,8 @@ import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import "./style/index.scss"
+import axios from 'axios'; /* 引入axios进行地址访问*/
+Vue.prototype.axios=axios
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
