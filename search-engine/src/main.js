7cbdbeb05f110d5ffde0fb1abc9c4c7afcf2fb7e
Ys <1250484624@qq.com>
2021-04-20 19:39:54 +0800

    添加登陆状态

--- search-engine/src/main.js
+++ search-engine/src/main.js
@@ -5,6 +5,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import "./style/index.scss"
 import axios from 'axios'; /* 引入axios进行地址访问*/
+import store from './store'
 Vue.prototype.axios=axios
 Vue.use(ElementUI)
 
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- search-engine/src/store/index.js
+++ search-engine/src/store/index.js
@@ -2,5 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 const store = new Vuex.Store({
+    state: {
+        login: false
+    },
+    mutations: {
+        toLogin (state) {
+            state.login=true
+        }
+    }
 })
 export default store
