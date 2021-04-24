5e5a20b5bdde178bab4b1dc3793db251d034bcdb
Ys <1250484624@qq.com>
2021-04-24 11:04:57 +0800

    完成视频搜索部分

---
+++ search-engine/src/components/Video/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <el-card class="box" shadow="hover">
+            <div>
+                {{videoInfo.file_name}}
+            </div>
+            <i
+                    :class="playIcon"
+                    class="playButton"
+                    @click="playVideo()"></i>
+            <p class="sentence_text" v-html="videoInfo.sentence_text"></p>
+
+        </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Video",
+        data() {
+            return {
+                playIcon:"el-icon-video-play"
+            }
+        },
+        methods: {
+            playVideo(){
+                this.$emit('playVideo', this.videoInfo); //父组件监听的名字必须是formSubmit
+            }
+        },
+        components: {},
+        mounted() {
+
+        },props:['videoInfo']
+    }
+</script>
+
+<style scoped>
+    .playButton {
+        font-size: 45px;
+        margin-right: 10px;
+        margin-top: 5px;
+        float: left;
+        cursor: pointer;
+        color: #409eff;
+        font-weight: bold;
+    }
+    .box {
+        width: 595px;
+        margin: 5px;
+        padding: 5px;
+        /*height: 140px;*/
+    }
+    .sentence_text{
+        margin-top: 5px;
+    }
+</style>
