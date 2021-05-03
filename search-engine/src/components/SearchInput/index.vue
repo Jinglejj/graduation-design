b42861b89ea5a46159dfdb2e2698c565e3c2ca38
yujinjiang <964494258@qq.com>
2021-05-03 18:47:46 +0800

    优化搜索方式

--- search-engine/src/components/SearchInput/index.vue
+++ search-engine/src/components/SearchInput/index.vue
@@ -1,32 +1,34 @@
 <template >
-  <el-input placeholder="请输入内容" :value="input" @input="(val)=>$emit('update:input',val)">
-      <template slot="prepend">
-        <el-upload
-          action="https://jsonplaceholder.typicode.com/posts/"
-          :auto-upload="false"
-          :show-file-list="false"
-          :on-change="(file)=>$emit('uploadFile',file)"
-        >
-          <i class="el-icon-picture-outline"></i>
-        </el-upload>
-      </template>
-      <el-button
-        slot="append"
-        icon="el-icon-search"
-        @click="$emit('search')"
-      ></el-button>
-    </el-input>
+  <el-input
+    placeholder="请输入内容"
+    type="text"
+    prefix-icon="el-icon-search"
+    @keyup.enter.native="$emit('search')"
+    :value="input"
+    @input="(val) => $emit('update:input', val)"
+  >
+    <template slot="append" style="backgorund-color:#409EFF">
+      <el-upload
+        action="https://jsonplaceholder.typicode.com/posts/"
+        :auto-upload="false"
+        :show-file-list="false"
+        :on-change="(file) => $emit('uploadFile', file)"
+      >
+        <i class="el-icon-picture-outline"></i>
+      </el-upload>
+    </template>
+  </el-input>
 </template>
 
 
 <script>
 export default {
   name: "SearchInput",
-  props:{
-      input:{
-          type:String,
-          required:true
-      }
-  }
+  props: {
+    input: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
