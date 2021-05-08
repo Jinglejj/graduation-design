bbb748de43e8485ac9df3b56026b87e5ceb1a2e7
Ys <1250484624@qq.com>
2021-05-08 21:01:39 +0800

    修改加载顺序

--- search-engine/src/views/audioPage/index.vue
+++ search-engine/src/views/audioPage/index.vue
@@ -2,23 +2,23 @@
     <div>
         <template v-if="audioInfo">
             <p class="resultsTips">为您搜索到{{totalCount}}条相关音频</p>
-            <AudioList :audio-info="audioInfo"  @func="getMsgFromSon" ref="audioList" />
-        </template>
-        <div class="block">
-            <el-pagination
-                    @current-change="handleCurrentChange"
-                    :current-page.sync="pageNum"
-                    :page-size="pageSize"
-                    :hide-on-single-page="hidePage"
-                    layout="prev, pager, next, jumper"
-                    :total="totalCount">
-            </el-pagination>
-        </div>
-        <template v-if="totalCount===0">
-            <p class="notFound">
-                抱歉没有找到与“<span>{{this.keyWord}}</span>”
-                相关的音频。
-            </p>
+            <AudioList :audio-info="audioInfo" @func="getMsgFromSon" ref="audioList"/>
+            <div class="block">
+                <el-pagination
+                        @current-change="handleCurrentChange"
+                        :current-page.sync="pageNum"
+                        :page-size="pageSize"
+                        :hide-on-single-page="hidePage"
+                        layout="prev, pager, next, jumper"
+                        :total="totalCount">
+                </el-pagination>
+            </div>
+            <template v-if="totalCount===0">
+                <p class="notFound">
+                    抱歉没有找到与“<span>{{this.keyWord}}</span>”
+                    相关的音频。
+                </p>
+            </template>
         </template>
     </div>
 </template>
@@ -27,28 +27,29 @@
     import AudioList from "@/components/AudioList";
     import {searchAudio} from "../../apis/search";
     import ResultMixins from "@/mixins/result-mixins"
+
     export default {
         name: "AudioPage",
-        mixins:[ResultMixins],
+        mixins: [ResultMixins],
         components: {AudioList},
         data() {
             return {
-                audioInfo:null,
+                audioInfo: null,
             }
         },
         methods: {
-            async search(){
-                const {data} = await searchAudio(this.keyWord,this.pageNum,this.pageSize);
-                if(data.code!==100){
+            async search() {
+                const {data} = await searchAudio(this.keyWord, this.pageNum, this.pageSize);
+                if (data.code !== 100) {
                     this.$message.error("查询出错音频，请更换关键词！")
                 }
-                this.audioInfo=data.map.data.results
+                this.audioInfo = data.map.data.results
                 //重新渲染计算
                 setTimeout(() => {
                     this.$refs.audioList.parseInfo();
                 }, 100);
-            },getMsgFromSon(data){
-                this.totalCount=data
+            }, getMsgFromSon(data) {
+                this.totalCount = data
             }
         }
     }
--- search-engine/src/views/videoPage/index.vue
+++ search-engine/src/views/videoPage/index.vue
@@ -1,95 +1,97 @@
 <template>
-  <div>
-    <template v-if="videoInfo">
-      <p class="resultsTips">为您搜索到{{totalCount}}条相关视频</p>
-      <Video
-        :videoInfo="video"
-        :key="video.id"
-        v-for="video in videoInfo"
-        v-on:playVideo="showDialog"
-      ></Video>
-    </template>
-    <template v-if="playFile">
-      <el-dialog
-        :title="playFile.file_name"
-        top="20vh"
-        :visible.sync="dialogVisible"
-        @close="closeDialog"
-        width="41%"
-      >
-        <video
-          width="740"
-          height="430"
-          ref="videoPlayer"
-          id="videoPlayer"
-          :src="
+    <div>
+        <template v-if="videoInfo">
+            <p class="resultsTips">为您搜索到{{totalCount}}条相关视频</p>
+            <Video
+                    :videoInfo="video"
+                    :key="video.id"
+                    v-for="video in videoInfo"
+                    v-on:playVideo="showDialog"
+            ></Video>
+        </template>
+        <template v-if="playFile">
+            <el-dialog
+                    :title="playFile.file_name"
+                    top="20vh"
+                    :visible.sync="dialogVisible"
+                    @close="closeDialog"
+                    width="41%"
+            >
+                <video
+                        width="740"
+                        height="430"
+                        ref="videoPlayer"
+                        id="videoPlayer"
+                        :src="
             'http://106.53.148.120:8989/download?fileName=' + playFile.file_name
           "
-          controls
-        ></video>
-      </el-dialog>
-    </template>
-    <div class="block">
-      <el-pagination
-        @current-change="handleCurrentChange"
-        :current-page.sync="pageNum"
-        :page-size="pageSize"
-        :hide-on-single-page="hidePage"
-        layout="prev, pager, next, jumper"
-        :total="totalCount"
-      >
-      </el-pagination>
+                        controls
+                ></video>
+            </el-dialog>
+            <div class="block">
+                <el-pagination
+                        @current-change="handleCurrentChange"
+                        :current-page.sync="pageNum"
+                        :page-size="pageSize"
+                        :hide-on-single-page="hidePage"
+                        layout="prev, pager, next, jumper"
+                        :total="totalCount"
+                >
+                </el-pagination>
+            </div>
+            <template v-if="totalCount===0">
+                <p class="notFound">
+                    抱歉没有找到与“<span>{{this.keyWord}}</span>”相关的视频。
+                </p>
+            </template>
+        </template>
+
     </div>
-    <template v-if="totalCount===0">
-      <p class="notFound">
-        抱歉没有找到与“<span>{{this.keyWord}}</span>”相关的视频。
-      </p>
-    </template>
-  </div>
 </template>
 
 <script>
-import { searchVideo } from "../../apis/search";
-import Video from "@/components/Video";
+    import {searchVideo} from "../../apis/search";
+    import Video from "@/components/Video";
 
-import ResultMixins from "@/mixins/result-mixins";
-export default {
-  name: "VideoPage",
-  mixins: [ResultMixins],
-  components: { Video },
-  data() {
-    return {
-      videoInfo: null,
-      playFile: null,
-      dialogVisible: false,
+    import ResultMixins from "@/mixins/result-mixins";
+
+    export default {
+        name: "VideoPage",
+        mixins: [ResultMixins],
+        components: {Video},
+        data() {
+            return {
+                videoInfo: null,
+                playFile: null,
+                dialogVisible: false,
+            };
+        },
+        methods: {
+            async search() {
+                const {data} = await searchVideo(
+                    this.keyWord,
+                    this.pageNum,
+                    this.pageSize
+                );
+                if (data.code !== 100) {
+                    this.$message.error("查询视频出错，请更换关键词！");
+                }
+                this.videoInfo = data.map.data.results;
+                this.totalCount = data.map.data.total;
+            },
+            showDialog(text) {
+                this.dialogVisible = true;
+                this.playFile = text;
+                setTimeout(() => {
+                    //不设置的话，获取不到videoPlayer元素
+                    this.$refs.videoPlayer.currentTime = parseInt(text.start_time / 1000);
+                }, 400);
+            },
+            closeDialog() {
+                this.playFile = null;
+            },
+        },
     };
-  },
-  methods: {
-    async search() {
-      const { data } = await searchVideo(
-        this.keyWord,
-        this.pageNum,
-        this.pageSize
-      );
-      if (data.code !== 100) {
-        this.$message.error("查询视频出错，请更换关键词！");
-      }
-      this.videoInfo = data.map.data.results;
-      this.totalCount = data.map.data.total;
-    },
-    showDialog(text) {
-      this.dialogVisible = true;
-      this.playFile = text;
-      setTimeout(() => {
-        //不设置的话，获取不到videoPlayer元素
-        this.$refs.videoPlayer.currentTime = parseInt(text.start_time / 1000);
-      }, 400);
-    },
-    closeDialog() {
-      this.playFile = null;
-    },
-  },
-};
 </script>
 
 <style scoped>
