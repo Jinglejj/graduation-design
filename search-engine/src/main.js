b44004ac34a6360bfdeb5c96a0bb0395a24288b7
Ys <1250484624@qq.com>
2021-05-12 20:15:41 +0800

    音频标题

--- search-engine/src/main.js
+++ search-engine/src/main.js
@@ -6,7 +6,9 @@ import 'element-ui/lib/theme-chalk/index.css';
 import "./style/index.scss"
 import axios from 'axios'; /* 引入axios进行地址访问*/
 import store from './store'
+import {transTime} from './utils/index'
 Vue.prototype.axios=axios
+Vue.prototype.$utils=transTime
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 import   './utils/dialogDrag'
